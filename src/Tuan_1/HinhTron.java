73c39cea1ff7d69dfdc70e4d726a276cb500d065
Zarayos <nguyenanhkiet60115@gmail.com>
2026-09-11 17:26:40 +0700

    Update HinhTron.java

--- src/Tuan_1/HinhTron.java
+++ src/Tuan_1/HinhTron.java
@@ -1,8 +1,52 @@
 package Tuan_1;
 
-public class HinhTron {
-	private double banKinh;
-	private ToaDo Tam;
-	public final double PI = 3.1416;
-	
+public class ToaDo {
+    private String ten;
+    private float x;
+    private float y;
+
+    // Phương thức khởi tạo không tham số
+    public ToaDo() {
+        this.ten = "O";
+        this.x = 0.0f;
+        this.y = 0.0f;
+    }
+
+    // Phương thức khởi tạo đầy đủ tham số
+    public ToaDo(String ten, float x, float y) {
+        this.ten = ten;
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getter và Setter
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s(%.2f, %.2f)", ten, x, y);
+    }
 }
+2.
